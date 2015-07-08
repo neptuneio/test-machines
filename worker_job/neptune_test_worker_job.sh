f0a3bd4044d99c0497205fc468005a95f1988875
Satish Talluri <satish@neptune.io>
2015-07-08 16:39:05 -0700

    Trigger webhook rule from worker job

--- worker_job/neptune_test_worker_job.sh
+++ worker_job/neptune_test_worker_job.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 sleep 600
+# Trigger webhook rule on staging
+curl -X POST "https://neptune-staging-env.herokuapp.com/api/v1/trigger/channel/webhook/3052843476f74f5db8f50d6708528d88/d816d2ca-9480-4043-b8c5-465d28baed66"
